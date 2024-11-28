1ca4f9b9ea5c9ab4e434a039b308991bddd297b6
ly <camly1652004@gmail.com>
2024-11-28 22:14:51 +0700

    capnhat

--- comment/models.py
+++ comment/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.conf  import settings
+from django.contrib.auth.models import User
 
 
 class Article(models.Model): #model bài viết
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    image=models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments") # xóa bài viết xóa luôn bình luận,tìm các bình luận thông qua từ khóa comments
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()  # Nội dung bình luận
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"Comment by {self.user.username} on {self.article.title}"
