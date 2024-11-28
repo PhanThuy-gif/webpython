84dc450786e1d7924c0222380ea2e537290fa6bc
ly <camly1652004@gmail.com>
2024-11-29 01:19:58 +0700

    capnhat

--- comment/models.py
+++ comment/models.py
@@ -6,12 +6,6 @@ class Article(models.Model): #model bài viết
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-    def __str__(self):
-        return self.title
-
-
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
