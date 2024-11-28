dc54fcdfb3b02523b234dc4e620b13f49ac61c90
ly <camly1652004@gmail.com>
2024-11-28 22:32:49 +0700

    cn

--- comment/views.py
+++ comment/views.py
@@ -7,14 +7,16 @@ def article_detail(request, pk):
     comments = article.comments.all()  # Lấy tất cả bình luận liên quan đến bài viết
     form = CommentForm()
 
-    if request.method == 'POST':
-        form = CommentForm(request.Post)
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = CommentForm(request.POST)
         if form.is_valid(): #Kiểm tra form có hợp lệ không
             comment = form.save(commit=False)
             comment.article = article # gắn bài viết vào bình luận
             comment.user = request.user  # Gắn bình luận với người dùng hiện tại
             comment.save()
             return redirect('article_detail', pk=article.pk)
+        else:
+            print(form.errors) #in ra lỗi form
 
     return render(request, 'comment/article_detail.html', {
         'article': article,
