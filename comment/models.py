033e804aed667f0fe3be7069f7c63766c679697e
ly <camly1652004@gmail.com>
2024-11-30 02:29:23 +0700

    capnhat

--- comment/models.py
+++ comment/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Article(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+from django.conf import settings
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title
-
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Comment by {self.author} on {self.article}"
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
--- comment/views.py
+++ comment/views.py
@@ -1,29 +1,15 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Article, Comment
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 from .forms import CommentForm
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
-def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    comments = article.comments.all()  # Lấy tất cả bình luận liên quan đến bài báo
-
+# Create your views here.
+def post(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm()
     if request.method == 'POST':
-        if request.user.is_authenticated:
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                comment.article = article
-                comment.author = request.user
-                comment.save()
-                return redirect('article_detail', pk=pk)
-        else:
-            return redirect('login')  # Chuyển hướng đến trang đăng nhập nếu người dùng chưa đăng nhập
-    else:
-        form = CommentForm()
-    return render(request, 'article_detail.html', {
-        'article': article,
-        'comments': comments,
-        'form': form,
-    })
-
-    
+        form = CommentForm(request.POST,author=request.user,post=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    return render(request, "comment/post.html", {"post":post, "form":form})
