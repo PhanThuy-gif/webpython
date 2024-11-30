510765baa2f140c2546b659ae7af55c370248304
ly <camly1652004@gmail.com>
2024-12-01 05:49:51 +0700

    cnhome.html

--- comment/urls.py
+++ comment/urls.py
@@ -3,10 +3,10 @@ from comment import views
 from django.views.generic import ListView, DetailView
 from comment.models import Post
 from django.contrib.auth import views as auth_views
-from django.urls import re_path
+
 
 
 urlpatterns = [
-    re_path(r'^article-detail/(?P<post_url>\d+)/$', views.article_detail, name='article_detail'),
+    path('article-detail/<str:post_url>/', views.article_detail, name='article_detail'),
     path('article-content/', views.article_content, name='article_content'),
 ]
