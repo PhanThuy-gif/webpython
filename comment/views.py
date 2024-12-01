1bf393ab0c346d994575a085fe661d92966bb7b5
ly <camly1652004@gmail.com>
2024-12-01 09:22:57 +0700

    cn lai forms

--- comment/views.py
+++ comment/views.py
@@ -4,14 +4,13 @@ from django.contrib.auth.decorators import login_required
 from comment.models import Comment
 from django.http import HttpResponseRedirect
 from home.models import Article
-
+@login_required
 def post_detail(request, article_link):
     post = get_object_or_404(Article, link=article_link)  
-    comments = CommentForm()
+    form = CommentForm()
     if request.method == 'POST':
-        comments= CommentForm(request.POST, author=request.user, post=post) 
-        if comments.is_valid():
-            comments.save()
+        form= CommentForm(request.POST, author=request.user, post=post) 
+        if form.is_valid():
+            form.save()
             return HttpResponseRedirect(request.path)
-    return render(request, "article_detail.html", {"post": post, "comments": comments})
- 
+    return render(request, "article_detail.html", {"post": post,"form ":form})
